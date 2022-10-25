17d13b51c7d4c5cea5e7015841132c08cdd0bac1
Burulai001 <abdraburulai@gmail.com>
2022-10-25 13:01:00 -0700

    New updates

--- spring-13-Data-CinemaLab/src/main/java/com/cydeo/Spring13DataCinemaLabApplication.java
+++ spring-13-Data-CinemaLab/src/main/java/com/cydeo/Spring13DataCinemaLabApplication.java
@@ -4,8 +4,11 @@ package com.cydeo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.flywaydb.core.Flyway;
+import org.flywaydb.core.api.output.MigrateResult;
 
 import javax.sql.DataSource;
+
 
 @SpringBootApplication
 public class Spring13DataCinemaLabApplication {
@@ -15,8 +18,8 @@ public class Spring13DataCinemaLabApplication {
 
 	}
 
-//	@Bean
-//	public MigrateResult migrateResult(DataSource dataSource){
-//		return Flyway.configure().baselineOnMigrate(true).dataSource(dataSource).load().migrate();
-//	}
+	@Bean
+	public MigrateResult migrateResult(DataSource dataSource){
+		return Flyway.configure().baselineOnMigrate(true).dataSource(dataSource).load().migrate();
+	}
 }
